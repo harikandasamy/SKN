875ac83e6bee191d11f28fb94704c836129bd9a9
harikandasamy <hariharan.kandasamy@gmail.com>
2025-05-22 23:19:40 -0700

    Update app.py

--- app.py
+++ app.py
@@ -257,7 +257,7 @@ with col1:
             st.session_state.userdata = result
             st.session_state.populateusers = 1
             st.session_state.two_factor = result[0].get("code")
-            if st.session_state.two_factor = -1
+            if st.session_state.two_factor == -1
                 result_div.success(result[0].get("code"))
             else:
                 result_div.success("Users populated successfully")
