c1fb2beaf77e74bf029cdd007c7029bdc62cadb4
harikandasamy <hariharan.kandasamy@gmail.com>
2025-05-26 22:08:44 -0700

    Update app.py

--- app.py
+++ app.py
@@ -203,7 +203,7 @@ def process_request(callno, types=None):
     search_term_val = st.session_state.get("search_term", "")
     twofactor_val = st.session_state.get("twofactor", -1)
    
-    msg, sttus, code = validate_user(user_id, "webadmin", -1)
+    (msg, sttus, code) = validate_user(user_id, "webadmin", -1)
 
     #result_div.success("what")
     #result_div.success(sttus)
